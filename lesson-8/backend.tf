8a4e97a503ec493d6e285d5479741f7b65da0abc
Petro Davydov <arturshopengauergik@gmail.com>
2026-04-15 11:32:56 +0000

    add correct 10

--- lesson-8/backend.tf
+++ lesson-8/backend.tf
@@ -3,7 +3,7 @@
 # terraform {
 #   backend "s3" {
 #     bucket         = "lesson-8-jenkins-state-bucket-001001"# Назва S3-бакета
-#     key            = "lesson-7/terraform.tfstate"   # Шлях до файлу стейту
+#     key            = "lesson-8/terraform.tfstate"   # Шлях до файлу стейту
 #     region         = "eu-west-1"                    # Регіон AWS
 #     dynamodb_table = "terraform-locks-jenkins"      # Назва таблиці DynamoDB
 #     encrypt        = true                           # Шифрування файлу стейту
