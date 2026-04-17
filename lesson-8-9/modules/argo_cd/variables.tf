184c66de1148c0a837df0cd7be7a5c69c267a879
Petro Davydov <arturshopengauergik@gmail.com>
2026-04-17 08:29:21 +0000

    check 4 add code to files

--- lesson-8-9/modules/argo_cd/variables.tf
+++ lesson-8-9/modules/argo_cd/variables.tf
@@ -1,1 +1,19 @@
 # lesson-8-9/modules/argo_cd/variables.tf
+
+variable "name" {
+  description = "Назва Helm-релізу"
+  type        = string
+  default     = "argo-cd"
+}
+
+variable "namespace" {
+  description = "K8s namespace для Argo CD"
+  type        = string
+  default     = "argocd"
+}
+
+variable "chart_version" {
+  description = "Версія Argo CD чарта"
+  type        = string
+  default     = "5.46.4" 
+}
