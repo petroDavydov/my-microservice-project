1ce1b0788cd86ff1b0974a73f101ac2b3e65bd6d
Petro Davydov <arturshopengauergik@gmail.com>
2026-04-18 07:55:38 +0000

    checl 9

--- lesson-8-9/backend.tf
+++ lesson-8-9/backend.tf
@@ -1,11 +1,11 @@
-# # /lesson-8/backend.tf
+# # /lesson-8-9/backend.tf
 
 # terraform {
 #   backend "s3" {
-#     bucket         = "lesson-8-jenkins-state-bucket-001001"# Назва S3-бакета
-#     key            = "lesson-8/terraform.tfstate"   # Шлях до файлу стейту
+#     bucket         = "lesson-8-9-jenkins-argo-cd-state-bucket-001001"# Назва S3-бакета
+#     key            = "lesson-8-9/terraform.tfstate"   # Шлях до файлу стейту
 #     region         = "eu-west-1"                    # Регіон AWS
-#     dynamodb_table = "terraform-locks-jenkins"      # Назва таблиці DynamoDB
+#     dynamodb_table = "terraform-locks-jenkins-argo-cd"      # Назва таблиці DynamoDB
 #     encrypt        = true                           # Шифрування файлу стейту
 #   }
 # }
--- lesson-8-9/main.tf
+++ lesson-8-9/main.tf
@@ -1,9 +1,9 @@
-# lesson-8/main.tf
+# lesson-8-9/main.tf
 # Підключаємо модуль для S3 та DynamoDB
 module "s3_backend" {
   source = "./modules/s3-backend/"                      # Шлях до модуля
-  bucket_name = "lesson-8-jenkins-state-bucket-001001"  # Ім'я S3-бакета
-  table_name  = "terraform-locks-jenkins"               # Ім'я DynamoDB
+  bucket_name = "lesson-8-9-jenkins-argo-cd-state-bucket-001001"  # Ім'я S3-бакета
+  table_name  = "terraform-locks-jenkins-argo-cd"               # Ім'я DynamoDB
 }
 
 
@@ -14,21 +14,21 @@ module "vpc" {
   public_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]         # Публічні підмережі
   private_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]         # Приватні підмережі
   availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]            # Зони доступності
-  vpc_name            = "vpc-lesson-8-jenkins"                                # Ім'я VPC
+  vpc_name            = "vpc-lesson-8-9-jenkins-argo-cd"                                # Ім'я VPC
 }
 
 
 # Підключаємо модуль ECR
 module "ecr" {
   source      = "./modules/ecr"
-  ecr_name    = "lesson-8-ecr-jenkins"
+  ecr_name    = "lesson-8-9-ecr-jenkins-argo-cd"
   scan_on_push = true
 }
 
 # Додаємо eks 
 module "eks" {
   source          = "./modules/eks"          
-  cluster_name    = "eks-cluster-lesson-8-jenkins" # Назва кластера
+  cluster_name    = "eks-cluster-lesson-8-9-jenkins-argo-cd" # Назва кластера
   subnet_ids      = module.vpc.private_subnets     # ID підмереж
   instance_type   = "t3.medium"                    # Тип інстансів
   desired_size    = 2                              # Бажана кількість нодів
